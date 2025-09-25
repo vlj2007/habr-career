ca8390b00cf02c667ffebd58045b575590f3d9c1
vladimir <vlj2007@yandex.ru>
2025-09-25 23:33:53 +0700

    I add a file class JsonWriteException

---
+++ src/main/java/my/code/exception/JsonWriteException.java
@@ -0,0 +1,7 @@
+package my.code.exception;
+
+public class JsonWriteException extends Throwable {
+    public JsonWriteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
