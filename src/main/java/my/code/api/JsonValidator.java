089b450fecda0e521b853dd408fc77ad1ebb6a9b
vladimir <vlj2007@yandex.ru>
2025-09-28 13:43:52 +0700

    I added javadoc for class JsonValidator.java

--- src/main/java/my/code/api/JsonValidator.java
+++ src/main/java/my/code/api/JsonValidator.java
@@ -1,5 +1,20 @@
 package my.code.api;
 
+/**
+ * Интерфейс для проверки валидации результата.
+ * Определяет метод для валидации.
+ *
+ * @author Vlj2007
+ * @version 0.1
+ */
+
 public interface JsonValidator {
+
+    /**
+     * Проверка валидации результата в файл с заданными параметрами.
+     *
+     * @param filename название файла
+     */
+
     boolean validate(String filename);
 }
