169e5409c71e51f3144983d31035ad992919f265
vladimir <vlj2007@yandex.ru>
2025-09-28 13:56:12 +0700

    I added javadoc for class JsonWriteException.java

--- src/main/java/my/code/exception/JsonWriteException.java
+++ src/main/java/my/code/exception/JsonWriteException.java
@@ -1,6 +1,19 @@
 package my.code.exception;
 
+/**
+ * Класс для обработки исключения записи результата в файл.
+ * Определяет метод для обработки исключения записи в файл.
+ */
+
 public class JsonWriteException extends Throwable {
+
+    /**
+     * Обработки исключения записи результата в файл.
+     *
+     * @param message текстовое сообщение.
+     * @param cause   исходное исключение.
+     */
+
     public JsonWriteException(String message, Throwable cause) {
         super(message, cause);
     }
