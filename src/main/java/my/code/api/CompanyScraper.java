9c2fb9c67d636d5dc88efe37793086bb7b1a4e3d
vladimir <vlj2007@yandex.ru>
2025-09-28 13:39:35 +0700

    I added javadoc for class CompanyScraper.java

--- src/main/java/my/code/api/CompanyScraper.java
+++ src/main/java/my/code/api/CompanyScraper.java
@@ -5,7 +5,22 @@ import my.code.model.Company;
 
 import java.util.List;
 
+/**
+ * Интерфейс для сбора данных с сайта.
+ * Определяет методы для сбора данных с сайта.
+ * @author Vlj2007
+ * @version 0.1
+ */
+
 public interface CompanyScraper {
+
+    /**
+     * Сбор данных с сайта с заданными параметрами.
+     *
+     * @param url адрес сайта
+     * @throws ScraperException если сайт не найден
+     */
+
     List<Company> scrape(String url) throws ScraperException;
 
     List<Company> scraper(String url) throws ScraperException;
